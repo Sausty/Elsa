0873ac394f7d06e466b77f78d4fd135e3e99facd
Sausty <36990678+Sausty@users.noreply.github.com>
2022-06-25 17:38:37 +0200

    [DOCS] Docs update

--- Elsa/src/ApplicationTypes.h
+++ Elsa/src/ApplicationTypes.h
@@ -11,31 +11,50 @@
 
 #include <Defines.h>
 
+/** @brief Represents a game instance, with all it's function pointers. **/
 typedef struct Game {
     struct {
+        /** @brief The X position of the window. */
         i16 PosX;
+        /** @brief The Y position of the window. */
         i16 PosY;
+        /** @brief The width of the window. */
         i32 Width;
+        /** @brief The height of the window. */
         i32 Height;
+        /** @brief The title position of the window. */
         const char* Name;
     } AppConfig;
 
+    /** @brief Called once at the beginning of the application. */
     b8 (*Init)(struct Game* game);
+    /** @brief Called once at the end of the application. */
     b8 (*Free)(struct Game* game);
+    /** @brief Called once every frame. */
     b8 (*Update)(struct Game* game);
+    /** @brief Called once every frame after update. */
     b8 (*Render)(struct Game* game);
+    /** @brief Called when the game window is resized. */
     void (*OnResize)(struct Game* game, u32 width, u32 height);
 } Game;
 
+/** @brief Represents the overall state of an Elsa application. */
 typedef struct ApplicationState {
+    /** @brief The game instance. */
     Game* game;
 
+    /** @brief The X position of the window. */
     i16 PosX;
+    /** @brief The Y position of the window. */
     i16 PosY;
+    /** @brief The width of the window. */
     i32 Width;
+    /** @brief The height of the window. */
     i32 Height;
+    /** @brief The title of the window. */
     const char* Name;
 
+    /** @brief Whether or not the application is running. */
     b8 Running;
 } ApplicationState;
 
