53746d22bb453303d933b114d077aab69de7d0f9
Sausty <MikuoH15TH@gmail.com>
2022-06-26 14:49:48 +0200

    [RENDERER] Started renderer backend

--- Elsa/src/Renderer/RendererBackend.c
+++ Elsa/src/Renderer/RendererBackend.c
@@ -1,11 +1,36 @@
 #include "RendererBackend.h"
 
+#include <Platform/Platform.h>
+
+#include "Metal/MetalBackend.h"
+#include "Vulkan/VulkanBackend.h"
+
 b8 RendererBackendCreate(RendererBackendAPI api, RendererBackend* out_renderer_backend)
 {
-    return true;
+    if (api == RENDERER_BACKEND_API_METAL) {
+        out_renderer_backend->API = RENDERER_BACKEND_API_METAL;
+        out_renderer_backend->Init = MetalRendererBackendInit;
+        out_renderer_backend->Shutdown = MetalRendererBackendShutdown;
+        out_renderer_backend->Resized = MetalRendererBackendResized;
+        out_renderer_backend->BeginFrame = MetalRendererBackendBeginFrame;
+        out_renderer_backend->EndFrame = MetalRendererBackendEndFrame;
+
+        return true;
+    } else if (api == RENDERER_BACKEND_API_VULKAN) {
+        out_renderer_backend->API = RENDERER_BACKEND_API_VULKAN;
+        out_renderer_backend->Init = VulkanRendererBackendInit;
+        out_renderer_backend->Shutdown = VulkanRendererBackendShutdown;
+        out_renderer_backend->Resized = VulkanRendererBackendResized;
+        out_renderer_backend->BeginFrame = VulkanRendererBackendBeginFrame;
+        out_renderer_backend->EndFrame = VulkanRendererBackendEndFrame;
+
+        return true;
+    }
+
+    return false;
 }
 
 void RendererBackendDestroy(RendererBackend* renderer_backend)
 {
-    
+    PlatformZeroMemory(renderer_backend, sizeof(RendererBackend));
 }
