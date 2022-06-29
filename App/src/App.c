95fe73806ee3c0415ed90713447dcc0e42ef1d6f
Sausty <MikuoH15TH@gmail.com>
2022-06-29 23:07:44 +0200

    [AUDIO] Restarted the audio system

--- App/src/App.c
+++ App/src/App.c
@@ -6,33 +6,27 @@
 #include <Audio/Audio.h>
 
 typedef struct AppData {
-    AudioClip* BGM;
+    
 } AppData;
 
 static AppData app;
 
 b8 GameInit(Game* game)
 {
-    app.BGM = AudioLoadClip("Assets/Audio/SyncamoreTheme.wav");
-    AudioSetClipVolume(app.BGM, 0.3f);
-    AudioLoopClip(app.BGM, true);
-    AudioPlayClip(app.BGM);
-
     ELSA_DEBUG("GameInit() called");
-
+	
     return true;
 }
 
 b8 GameFree(Game* game)
 {
-    AudioFreeClip(app.BGM);
-
+	
     return true;
 }
 
 b8 GameUpdate(Game* game)
 {
-
+	
     return true;
 }
 
